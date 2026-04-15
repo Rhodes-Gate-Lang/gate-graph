c5e2aa88faeba96f59497e19ef0c68955785beb1
Apollo Popowitz <apollo@MacBook-Pro-45.local>
2026-04-15 12:55:05 -0500

    created grapher.java file

---
+++ grapher.java
@@ -0,0 +1,14 @@
+<!-- Maven -->
+<dependency>
+  <groupId>com.google.protobuf</groupId>
+  <artifactId>protobuf-java</artifactId>
+  <version>3.25.0</version>
+</dependency>
+<!-- JavaFX -->
+<dependency>
+  <groupId>org.openjfx</groupId>
+  <artifactId>javafx-web</artifactId>
+  <version>21</version>
+</dependency>
+
+
